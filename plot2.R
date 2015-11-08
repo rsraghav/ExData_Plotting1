a87740695233035378e51e876307156bd097fcb7
Rajesh S. Raghavan <RajeshRaghavan@acm.org>
2015-11-08 14:59:23 -0500

    Exploratory Data Analysis Course Project 1 initial version

---
+++ plot2.R
@@ -0,0 +1,27 @@
+electric <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+head(electric)
+elecD <- electric$Date
+elecT <- electric$Time
+elecDT <- electric[,1:2]
+head(elecDT)
+elecD <- as.Date(elecDT[,1],"%d/%m/%Y")
+head(elecD)
+electric[,1] <- elecD
+head(electric)
+elecfebone07 <- subset(electric,electric$Date=="2007-02-01")
+elecfebtwo07 <- subset(electric,electric$Date=="2007-02-02")
+elecfeb <- rbind(elecfebone07,elecfebtwo07)
+elecfebDT <- paste(elecfeb$Date,elecfeb$Time)
+head(elecfebDT)
+p <- as.POSIXct(elecfebDT)
+head(p)
+head(elecfeb)
+ques <- elecfeb$Global_active_power=="?"
+elecfeb$Global_active_power[ques] <- NA
+elecfebKW <- as.numeric(elecfeb$Global_active_power) / 1000
+png(file = "plot1.png")
+hist(elecfebKW, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red", breaks = 15)
+dev.off()
+png(file = "plot2.png")
+plot(p,elecfebKW,type = "l",xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
